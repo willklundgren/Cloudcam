82c49455c0233b0f5b77da1637c98a42b756bb80
Will Lundgren <willklundgren@gmail.com>
2020-05-18 20:48:51 -0700

    Images can be sent from video feed to interpreter

--- app/main.py
+++ app/main.py
@@ -5,30 +5,14 @@ import cv2
 import numpy as np
 from VideoStream import VideoStream
 
-# Set minimum confidence level required to draw bounding box; default is 70%
-minimum_confidence = 0.70
+# Set minimum confidence level required to draw bounding box; default is 60%
+minimum_confidence = 0.60
 
 # Read label file and load labels into a variable
 with open( "./Sample_TFLite_model/labelmap.txt" ) as file:
     labels = [ line.strip() for line in file.readlines() ]
     if labels[0] == '???':
         del(labels[0])
-
-######################################################
-
-# Getting the video stream
-video_handler = VideoStream()
-
-for frame in video_handler.capture_stream():
-
-    image = frame.array
-    cv2.imwrite("./video_stream_test.jpg", image)
-
-    # Clear stream for next frame
-    video_handler.array_capture.truncate(0)
-    time.sleep(3)
-
-#######################################################
 
 print("Loading model into interpreter...")
 interpreter = Interpreter(model_path = "./Sample_TFLite_model/detect.tflite")
@@ -39,41 +23,59 @@ output_details = interpreter.get_output_details()
 model_input_height = input_details[0]["shape"][1]
 model_input_width = input_details[0]["shape"][2]
 
+# Preparing model
 interpreter.allocate_tensors()
 
-print("Performing image pre-processing...")
-image_path = "./BusinessWoman3.jpg" # Change accordingly
-image = cv2.imread( image_path )
-image_rgb = cv2.cvtColor( image, cv2.COLOR_BGR2RGB )
-image_rows, image_columns, image_color_channels = image.shape # Get original image height / width information
-image_resized = cv2.resize( image_rgb, ( model_input_width , model_input_height ) ) # Resize image to fit model's required input shape
-model_input_data = np.expand_dims( image_resized , axis=0 )
+######################################################
 
-print("Performing inference...")
-interpreter.set_tensor( input_details[0]["index"] , model_input_data )
-interpreter.invoke() # Executes the inference
+# Getting the video stream
+video_handler = VideoStream()
+frame_counter = 0
 
-boxes = interpreter.get_tensor(output_details[0]['index'])[0]
-classes = interpreter.get_tensor(output_details[1]["index"])[0]
-scores = interpreter.get_tensor(output_details[2]["index"])[0]
+for frame in video_handler.capture_stream():
 
-for i in range( len(scores) ):
-    object_detected = labels[ int(classes[i]) ]
-    confidence = scores[i]
+    image = frame.array
+    print("Performing image pre-processing...")
+    # image_path = "./BusinessWoman3.jpg" # Change accordingly
+    # image = cv2.imread( image_path )
+    image_rgb = cv2.cvtColor( image, cv2.COLOR_BGR2RGB )
+    image_rows, image_columns, image_color_channels = image.shape # Get original image height / width information
+    image_resized = cv2.resize( image_rgb, ( model_input_width , model_input_height ) ) # Resize image to fit model's required input shape
+    model_input_data = np.expand_dims( image_resized , axis=0 )
 
-    if confidence > minimum_confidence:
+    print("Performing inference...")
+    interpreter.set_tensor( input_details[0]["index"] , model_input_data )
+    interpreter.invoke() # Executes the inference
 
-        print("Object:", object_detected)
-        print("Confidence:", scores[i])
-        print("")
+    boxes = interpreter.get_tensor(output_details[0]['index'])[0]
+    classes = interpreter.get_tensor(output_details[1]["index"])[0]
+    scores = interpreter.get_tensor(output_details[2]["index"])[0]
 
-        # Get x and y coordinates of bounding box, correcting for inference results that lay off the original image
-        y_lower = int( max(1, (boxes[i][0] * image_rows) ) )
-        y_upper = int( max(image_rows , (boxes[i][2] * image_rows) ) )
-        x_lower = int( max(1, (boxes[i][1] * image_columns) ) )
-        x_upper = int( max(image_columns , (boxes[i][3] * image_columns) ) )
+    for i in range( len(scores) ):
+        object_detected = labels[ int(classes[i]) ]
+        confidence = scores[i]
 
-        # Draw bounding box on the image
-        cv2.rectangle(image, (x_lower, y_lower), (x_upper, y_upper), (255, 0, 0), 2)
-        # cv2.imshow("Inference Result", image)
-        cv2.imwrite("./cv2_test_image.jpg", image) # for saving the image
+        if confidence > minimum_confidence and object_detected == "person":
+
+            frame_counter += 1
+
+            print("Object:", object_detected)
+            print("Confidence:", scores[i])
+            print("")
+
+            # Get x and y coordinates of bounding box, correcting for inference results that lay off the original image
+            y_lower = int( max(1, (boxes[i][0] * image_rows) ) )
+            y_upper = int( max(image_rows , (boxes[i][2] * image_rows) ) )
+            x_lower = int( max(1, (boxes[i][1] * image_columns) ) )
+            x_upper = int( max(image_columns , (boxes[i][3] * image_columns) ) )
+
+            # Draw bounding box on the image
+            cv2.rectangle(image, (x_lower, y_lower), (x_upper, y_upper), (255, 0, 0), 2)
+            # cv2.imshow("Inference Result", image)
+            cv2.imwrite( f"./cv2_test_image{frame_counter}.jpg", image) # for saving the image
+
+    # cv2.imwrite("./video_stream_test.jpg", image)
+
+    # Clear stream for next frame
+    video_handler.array_capture.truncate(0)
+    time.sleep(3)
